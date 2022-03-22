0ec4c83450c3bc89ebf2641b6821098e201ae202
Tony <59328777+Technut10@users.noreply.github.com>
2022-03-22 15:01:07 +0000

    Format text for clarity.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ print("Welcome to the Split bill and tip calculator.")
 
 sleep(2)
 
-totalBill = input("What was the total of the bill? ")
+totalBill = input("What is the total of the bill? ")  
 totalBill = float(totalBill)
 billSplit = input("How many people to split the bill? ")
 billSplit = float(billSplit)
@@ -17,11 +17,12 @@ tipPercentage = input("What percentage tip would you like to give? ")
 
 tipPercentage = float(tipPercentage) / 100
 
-tipAmountAfterSplit = costPerPerson * tipPercentage 
-
+tipAmountAfterSplit = costPerPerson * tipPercentage
 
 totalToPay = costPerPerson + tipAmountAfterSplit
 
 totalToPay = round(totalToPay, 2)
 
 print(f"Each person should pay {totalToPay}")
+sleep(2)
+print("Thank you!")
